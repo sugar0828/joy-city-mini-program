6e5167ea786cf12a463e3e4c0342c1e16c48b810
sugar <shuguang@1506373411189760.onaliyun.com>
2020-03-08 23:52:53 +0800

    add npm + vant

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,40 @@
 //index.js
 //获取应用实例
 const app = getApp()
+import Dialog from '@vant/weapp/dialog/dialog'
 
 Page({
   data: {
     motto: '欢迎进入扬州御龙湾商业广场',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    carNo: '苏C...',
+    show: false,
+    actions: [
+      {
+        name: '选项'
+      },
+      {
+        name: '选项'
+      },
+      {
+        name: '选项',
+        subname: '副文本',
+        openType: 'getPhoneNumber'
+      }
+    ]
+  },
+  checkCost() {
+    Dialog.alert({
+      title: '提示',
+      message: '没有查到泊车信息，如号牌无误，请前往出口处人工缴费！'
+    })
+  },
+  toParkRecord() {
+    wx.navigateTo({
+      url: '/pages/parkInfo/park',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,7 +42,21 @@ Page({
       url: '../logs/logs'
     })
   },
+  showSheet() {
+    this.setData({
+      show: true
+    })
+  },
+  showDialog() {
+    Dialog.alert({
+      title: '标题',
+      message: '弹窗内容'
+    }).then(() => {
+      // on close
+    });
+  },
   onLoad: function () {
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -42,6 +83,12 @@ Page({
         }
       })
     }
+  },
+  onClose() {
+    this.setData({ show: false });
+  },
+  onSelect(event) {
+    console.log(event.detail);
   },
   getUserInfo: function(e) {
     console.log(e)
