3a84dc4ddc6e03492e92083a98f1ddb088863720
sugar <shuguang@1506373411189760.onaliyun.com>
2020-03-12 03:41:13 +0800

    register

---
+++ pages/register/register.js
@@ -0,0 +1,87 @@
+// pages/register/register.js
+import Toast from '@vant/weapp/toast/toast.js';
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    name: '',
+    phone: '',
+    address: '',
+    sex: '1',
+    agree: true
+  },
+  onChangeSex(event) {
+    this.setData({
+      sex: event.detail
+    });
+  },
+  toPointDesc() {
+    wx.navigateTo({
+      url: '/pages/pointDesc/pointDesc',
+    })
+  },
+  submitRegister() {
+    Toast.loading({
+      mask: false,
+      message: '提交中...'
+    });
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
